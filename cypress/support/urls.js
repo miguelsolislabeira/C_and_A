e26b339ccae0254eb2666167234f14861de1cedd
Miguel Solís <miguel.solis.lb@gmail.com>
2023-05-29 11:49:35 +0200

    Created urls.js file to avoid hardcoding within the test

---
+++ cypress/support/urls.js
@@ -0,0 +1,6 @@
+export const urls = {
+    Home: 'https://www.c-and-a.com',
+    Shop: '/es/es/shop',
+    SignUp: '/webapp/wcs/stores/servlet/account/registration?storeId=10155&langId=-5',
+    MyAccountAfterRegistration: '/webapp/wcs/stores/servlet/account/overview?storeId=10155&langId=-5'
+}
