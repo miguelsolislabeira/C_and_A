ee50bca62f68976cf92b4554f552cb5b154d0d26
Miguel Solís <miguel.solis.lb@gmail.com>
2023-05-29 10:32:19 +0200

    Created Sign up OK test

---
+++ cypress/e2e/signup.cy.js
@@ -0,0 +1,42 @@
+const accessPage = require("../pageObjects/AccessPage");
+const myAccountPage = require("../pageObjects/MyAccountPage");
+const signUpPage = require("../pageObjects/SignUpPage");
+const homePage = require("../pageObjects/homePage");
+
+
+describe("Sign Up", () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('The user can register on the site', () => {
+
+        homePage.clickOnAcceptCookiesButton();
+        homePage.clickOnspanishLanguageOption();
+        homePage.clickOnConfirmLanguageButton();
+        cy.url().should('eq','https://www.c-and-a.com/es/es/shop');
+        homePage.getLogo().should('be.visible');
+        homePage.clickOnAccountIcon();
+        
+        accessPage.clickOnRegistrateAhoraLink();
+        cy.url().should('eq','https://www.c-and-a.com/webapp/wcs/stores/servlet/account/registration?storeId=10155&langId=-5');
+        signUpPage.getNuevoClienteHeader().should('be.visible');
+        
+        signUpPage.checkGender("Mrs");
+        signUpPage.enterFirstName("Test");
+        signUpPage.enterLastName("Test");
+        signUpPage.enterEmailAddress("test1471182_@test.com");
+        signUpPage.enterPassword("felipe123!A");
+        signUpPage.checkPrivacyCheckbox();
+        signUpPage.clickOnRegisterButton();
+        
+        cy.url().should('contains', 'account');
+        myAccountPage.getSignUpSuccessMessage().contains('Tu cuenta de cliente se ha creado correctamente.')
+            .should('be.visible');
+        
+
+    });
+
+
+});
