8f6bfc31ff13be98f5f0f6d94d1b44988cad4a07
Miguel Solís <miguel.solis.lb@gmail.com>
2023-05-29 11:50:57 +0200

    Deleted hardcoded data within the sign up test

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -2,6 +2,8 @@ const accessPage = require("../pageObjects/AccessPage");
 const myAccountPage = require("../pageObjects/MyAccountPage");
 const signUpPage = require("../pageObjects/SignUpPage");
 const homePage = require("../pageObjects/homePage");
+import {getRandomString,  getRandomEmail, getRandomPassword} from '../support/utils';
+import {urls} from '../support/urls';
 
 
 describe("Sign Up", () => {
@@ -15,23 +17,23 @@ describe("Sign Up", () => {
         homePage.clickOnAcceptCookiesButton();
         homePage.clickOnspanishLanguageOption();
         homePage.clickOnConfirmLanguageButton();
-        cy.url().should('eq','https://www.c-and-a.com/es/es/shop');
+        cy.url().should('contains', urls.Shop);
         homePage.getLogo().should('be.visible');
         homePage.clickOnAccountIcon();
         
         accessPage.clickOnRegistrateAhoraLink();
-        cy.url().should('eq','https://www.c-and-a.com/webapp/wcs/stores/servlet/account/registration?storeId=10155&langId=-5');
+        cy.url().should('contain',urls.SignUp);
         signUpPage.getNuevoClienteHeader().should('be.visible');
         
         signUpPage.checkGender("Mrs");
-        signUpPage.enterFirstName("Test");
-        signUpPage.enterLastName("Test");
-        signUpPage.enterEmailAddress("test1471182_@test.com");
-        signUpPage.enterPassword("felipe123!A");
+        signUpPage.enterFirstName(getRandomString());
+        signUpPage.enterLastName(getRandomString());
+        signUpPage.enterEmailAddress(getRandomEmail());
+        signUpPage.enterPassword(getRandomPassword());
         signUpPage.checkPrivacyCheckbox();
         signUpPage.clickOnRegisterButton();
         
-        cy.url().should('contains', 'account');
+        cy.url().should('contain', urls.MyAccountAfterRegistration);
         myAccountPage.getSignUpSuccessMessage().contains('Tu cuenta de cliente se ha creado correctamente.')
             .should('be.visible');
         
