a15abd1832b5f90f4947b76bb941a1b98c1b034d
Miguel Solís <miguel.solis.lb@gmail.com>
2023-05-29 20:55:00 +0200

    Renamed AccessPage to LoginPage and created methods

---
+++ cypress/pageObjects/LoginPage.js
@@ -0,0 +1,31 @@
+class LoginPage{
+
+    registrateAhoraLink = 'Regístrate ahora';
+    emailInput = 'input[data-testid = "myaccount_login_email"]';
+    passwordInput = 'input[data-testid = "myaccount_login_password"]';
+    loginButton = 'button[data-qa = "LoadingButton"]';
+    failedLoginNotification = 'div[data-qa = "InlineNotification"] div';
+
+    clickOnRegistrateAhoraLink(){
+        cy.contains(this.registrateAhoraLink).click();
+    }
+
+    enterEmailAddress(emailAddress){
+        cy.get(this.emailInput).clear().type(emailAddress);
+    }
+
+    enterPassword(password){
+        cy.get(this.passwordInput).clear().type(password);
+    }
+
+    clickOnLoginButton(){
+        cy.get(this.loginButton).click();
+    }
+
+    getfailedLoginNotification(){
+        return cy.get(this.failedLoginNotification).eq(1);
+    }
+
+   
+}
+module.exports = new LoginPage();
