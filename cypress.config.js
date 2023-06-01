3dad3f2df8424487700ac16893bddedd9241dc59
Miguel Solís La Beira <miguel.solis.lb@gmail.com>
2023-06-01 19:40:23 +0100

    Increased timeout globally

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://c-and-a.com' 
+    baseUrl: 'https://c-and-a.com',
+    defaultCommandTimeout: 6000,
   },
 });
