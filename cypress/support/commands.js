e888ff796f68af49c3dd1d8de9d41e2b04c23b0d
Miguel Solís La Beira <miguel.solis.lb@gmail.com>
2023-06-01 19:43:10 +0100

    Added command to handle undesired requests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,9 +3,19 @@ import loginPage from "../pageObjects/LoginPage";
 Cypress.Commands.add('login', (emailAddress, password) => {
   loginPage.enterEmailAddress(emailAddress);
   loginPage.enterPassword(password);
+  cy.interceptRequests();
   loginPage.clickOnSessionButton();
 });
 
 Cypress.Commands.add('logout', () => {
   loginPage.clickOnSessionButton();
+});
+
+Cypress.Commands.add('interceptRequests', () => {
+  cy.intercept({ url: '**', method: 'GET' }, (req) => {
+    if (!req.url.startsWith('https://c-and-a.com')) {
+      req.reply({ status: 200, body: 'Request blocked' })
+    }
+  });
+
 });
